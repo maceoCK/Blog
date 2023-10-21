962b00fb33ed8a807ba2ada9389141f64c5c7fe4
maceoCK <maceo@vt.edu>
2023-10-21 14:21:13 -0400

    populated kahn's ago

--- Blog_assets/Kahn's Algorithm.py
+++ Blog_assets/Kahn's Algorithm.py
@@ -1,20 +1,62 @@
 from collections import defaultdict
 
+# Kahn's algorithm for topological sorting
+class Graph:
+    def __init__(self, vertices) -> None:
+        self.graph = defaultdict(list)
+        self.V = 0  # Initialize the number of vertices to 0
+
+    def addEdge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append(v)
+        if v not in self.graph:
+            self.graph[v] = []
+        if u != v:  # Avoid self-loops
+            self.V = len(self.graph)  # Update the number of vertices
+
+    def topologicalSort(self):
+        in_degree = [0] * self.V
+        for i in self.graph:
+            for j in self.graph[i]:
+                in_degree[j] += 1
+
+        queue = []
+        for i in range(self.V):
+            if in_degree[i] == 0:
+                queue.append(i)
+
+        cnt = 0
+        finalSort = []
+
+        while queue:
+            u = queue.pop(0)
+            finalSort.append(u)
+            for i in self.graph[u]:
+                in_degree[i] -= 1
+                if in_degree[i] == 0:
+                    queue.append(i)
+            cnt += 1
+
+        if cnt != self.V:
+            print("There is a cycle within the graph")
+        else:
+            print(finalSort)
+
 sampleGraph = {"Calc3": ["Calc1", "Calc2", "PreCalc"],
                "English": [],
                "Computer Science": ["Calc2"],
                "Machine Learning": ["Computer Science"],
                "Technical Writing": ["English"]}
 
-def countPreReqs(sampleGraph):
-    preReqCount = defaultdict(int)
+def graphMaker(adictionaryGraph):
+    finalGraph = Graph(0)
+    for key, value in adictionaryGraph.items():
+        finalGraph.V += 1  # Increment the number of vertices
+        for v in value:
+            finalGraph.addEdge(key, v)
 
-    for course, prerequisites in sampleGraph.items():
-        preReqCount[course]  # Initialize the course with 0 prerequisites
-        for prereq in prerequisites:
-            preReqCount[prereq] += 1
+    return finalGraph
 
-    return preReqCount
-
-preReqCounts = countPreReqs(sampleGraph)
-print(preReqCounts)
+endGraph = graphMaker(sampleGraph)
+endGraph.topologicalSort()
